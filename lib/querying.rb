261252c4bf4efe4a457b67e983025d81d94e279a
Sara Tibbetts <sxt4866@rit.edu>
2018-01-24 15:28:52 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,7 +1,7 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  <<-SQL
-    SELECT title, year
-    FROM books
-    ORDER BY year
-  SQL
+  # <<-SQL
+  #   SELECT title, year
+  #   FROM books
+  #   ORDER BY year
+  # SQL
 end
