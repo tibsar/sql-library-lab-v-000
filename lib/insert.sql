672187428de2fc4dbff2329c457421c3ebe8ce1c
Sara Tibbetts <sxt4866@rit.edu>
2018-01-24 15:17:21 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,3 @@
 INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Harry Potter", 1, 1), (2, "Lord of the Rings", 2, 2);
 
-INSERT INTO books (id, title, year, series_id) VALUES (1, "Harry Potter and the Sorcerer's Stone", 2004, 1)
+INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1), (2, "A Clash of Kings", 1998, 1), (3, "A Storm of Swords", 2000, 1), (4, "First Book", 2002, 2), (5, "Second Book", 2003, 2), (6, "Third Book", 2005, 2);
