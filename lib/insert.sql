57806ca116787a739812302bf8a7f3e9f5c56874
Sara Tibbetts <sxt4866@rit.edu>
2018-01-24 15:15:31 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES ()
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Harry Potter", 1, 1), (2, "Lord of the Rings", 2, 2)
