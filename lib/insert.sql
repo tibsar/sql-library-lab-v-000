322c3a8de4ca15571fb70e24d8d4beb86129450f
Sara Tibbetts <sxt4866@rit.edu>
2018-01-24 15:18:57 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,5 +2,4 @@ INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Harry Potter"
 
 INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1), (2, "A Clash of Kings", 1998, 1), (3, "A Storm of Swords", 2000, 1), (4, "First Book", 2002, 2), (5, "Second Book", 2003, 2), (6, "Third Book", 2005, 2);
 
-INSERT INTO characters (id, name, species, motto, series_id, author_id) VALUES (1, "Harry Potter", "human", "Expecto Patronum!", 1, 1); 
-
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1, 1), (2, "Tyrion Lanister", "A Lanister always pays is debts", "human", 1, 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1), (4, "Eddard Stark", "Winter is coming", "human", 1, 1);
