5a5a8a701ef32d20221f5f82fda70d4654fce055
Sara Tibbetts <sxt4866@rit.edu>
2018-01-24 15:18:42 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,11 +2,5 @@ INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Harry Potter"
 
 INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1), (2, "A Clash of Kings", 1998, 1), (3, "A Storm of Swords", 2000, 1), (4, "First Book", 2002, 2), (5, "Second Book", 2003, 2), (6, "Third Book", 2005, 2);
 
-CREATE TABLE characters (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  species TEXT,
-  motto TEXT,
-  series_id INTEGER,
-  author_id INTEGER
-);
+INSERT INTO characters (id, name, species, motto, series_id, author_id) VALUES (1, "Harry Potter", "human", "Expecto Patronum!", 1, 1); 
+
