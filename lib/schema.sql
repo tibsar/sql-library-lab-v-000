41ed0450d77cb9ec3d258014f1599db4d3bcc618
Sara Tibbetts <sxt4866@rit.edu>
2018-01-24 15:07:03 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,8 +1,10 @@
 CREATE TABLE series (
-  id INTEGER PRIMARY KEY,  
+  id INTEGER PRIMARY KEY,
   title STRING,
   author_id INTEGER,
   subgenre_id INTEGER
 );
 
-
+CREATE TABLE subgenres(
+  name STRING,
+); 
